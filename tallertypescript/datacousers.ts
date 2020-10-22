ac1c24c33974cfde05a58831e28d5caa28b5138b
jdpinilla <60227087+jdpinilla@users.noreply.github.com>
2020-10-22 00:58:13 -0500

    Add files via upload

---
+++ tallertypescript/datacousers.ts
@@ -0,0 +1,7 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de Sw", "Sergio Velasquez", 4),
+  new Course("Infraestructura computacional", "Hector Castro", 2),
+  new Course("Sistemas Transaccionales", "German Vasquez", 2)
+]
